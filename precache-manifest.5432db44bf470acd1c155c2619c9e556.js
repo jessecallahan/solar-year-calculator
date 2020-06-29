92ec660825ff80d4a8333ca47d145a0ac8352855
Jesse Callahan <jessetylercallahan@gmail.com>
2020-06-29 14:18:16 -0700

    Updates

---
+++ precache-manifest.5432db44bf470acd1c155c2619c9e556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be90e6ab989d52e71c74c04b46b7cf83",
+    "url": "/fireside-chat/index.html"
+  },
+  {
+    "revision": "51d36e354f02e2a42db5",
+    "url": "/fireside-chat/static/css/main.c34a4f03.chunk.css"
+  },
+  {
+    "revision": "7ba04588c72a1a78bd6e",
+    "url": "/fireside-chat/static/js/2.2a66b41e.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/fireside-chat/static/js/2.2a66b41e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d36e354f02e2a42db5",
+    "url": "/fireside-chat/static/js/main.0f697b55.chunk.js"
+  },
+  {
+    "revision": "3796699de69d8bc4e4fb",
+    "url": "/fireside-chat/static/js/runtime-main.e44db665.js"
+  }
+]);
